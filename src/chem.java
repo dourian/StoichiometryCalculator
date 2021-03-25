2a85203d7b0333bbac465054c4ef690ae5319fc7
dourian <75771131+dourian@users.noreply.github.com>
2021-03-24 21:48:50 -0400

    update

--- src/chem.java
+++ src/chem.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.math.*;
 // hi
 // hello
+// whats up
 public class chem extends constants{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
