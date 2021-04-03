07e623a070b92c6b5d6b1d32e38e7d4920001215
Sarina Li <sarinajin.li@gmail.com>
2021-04-02 21:27:10 -0400

    p2x

--- src/particlestoX.java
+++ src/particlestoX.java
@@ -1,4 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class particlestoX extends constants{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static StringTokenizer st;
+	static double p2mole() throws IOException {
+		System.out.println("Enter the decimal before the mulitplication of 10eX (i.e. 4.5 * 10e4 -> enter only 4.5)");
+		double decimal = readDouble();
+//		System.out.println("Enter the exponent of the 10 (i.e")
+		return 0.0;
+	}
+    public static void particles2Grams() {
+    }
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
 
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
 }
